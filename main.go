a421085d5e1f6d12213739b46f370e736dbcca35
Brady-Higgins <braydonhig@gmail.com>
2026-07-08 18:33:04 -0500

    syntax

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/Brady-Higgins/turtle/cmd"
-	"github.com/Brady-Higgins/turtle/internal/cloudflare_client"
 )
 
 func main() {
@@ -19,9 +16,9 @@ func main() {
 	////fmt.Println(d)
 	//err = c.DeleteDNSRecord(d, ctx)
 	//fmt.Println(err)
-	err := cloudflare_client.CreateTunnelDNSRecord("example", "plotsearcher.com")
-	if err != nil {
-		fmt.Println(err)
-	}
+	//err := cloudflare_client.CreateTunnelDNSRecord()
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
 
 }
