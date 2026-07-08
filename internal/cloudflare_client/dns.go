9eb56b6899437f8d0c2549429c9b3dc833b662de
Brady-Higgins <braydonhig@gmail.com>
2026-07-08 12:24:25 -0500

    add getDNSRecord method

--- internal/cloudflare_client/dns.go
+++ internal/cloudflare_client/dns.go
@@ -2,12 +2,19 @@ package cloudflare_client
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"os"
 
 	"github.com/cloudflare/cloudflare-go/v7"
 	"github.com/cloudflare/cloudflare-go/v7/dns"
 	"github.com/cloudflare/cloudflare-go/v7/option"
 )
+
+type DnsRecord struct {
+	Id         string `json:"id"`
+	RecordType string `json:"type"`
+}
 
 type cloudflareClient struct {
 	Cli *cloudflare.Client
@@ -31,10 +38,38 @@ func (c *cloudflareClient) NewDNSRecord(ctx context.Context) error {
 			Type:    cloudflare.F(dns.ARecordTypeA),
 			Content: cloudflare.F(os.Getenv("CLOUD_IP")),
 			Proxied: cloudflare.F(true), // proxy it
+			Comment: cloudflare.F("Turtle CLI"),
 		},
 	})
 	if err != nil {
 		return err
 	}
+	fmt.Println("New DNS Record Created Successfully")
 	return nil
 }
+
+func (c *cloudflareClient) GetDNSRecord(ctx context.Context) (*DnsRecord, error) {
+	//_, err := c.Cli.DNS.Records.Get(ctx)
+	resp, err := c.Cli.DNS.Records.List(ctx, dns.RecordListParams{
+		ZoneID: cloudflare.F(os.Getenv("CLOUDFLARE_ZONE_ID")),
+		Comment: cloudflare.F(dns.RecordListParamsComment{
+			Contains: cloudflare.F("Turtle CLI"),
+		}),
+	})
+	if err != nil {
+		return nil, err
+	}
+	if resp == nil {
+		return nil, fmt.Errorf("No current Turtle CLI created DNS Records.")
+	}
+	d := DnsRecord{}
+	err = json.Unmarshal([]byte(resp.Result[0].JSON.RawJSON()), &d)
+	if err != nil {
+		return nil, err
+	}
+	return &d, nil
+}
+
+//func (c *cloudflareClient) deleteDNSRecord(recordName string, recordType string, ctx context.Context) error {
+//	_, err := c.Cli.DNS.Records.Delete(ctx,)
+//}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/Brady-Higgins/turtle/cmd"
 	"github.com/Brady-Higgins/turtle/internal/cloudflare_client"
@@ -11,5 +12,13 @@ func main() {
 	cmd.Execute()
 	c := cloudflare_client.New()
 	ctx := context.Background()
-	c.NewDNSRecord(ctx)
+	//err := c.NewDNSRecord(ctx)
+	//fmt.Println(err)
+	d, err := c.GetDNSRecord(ctx)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(d)
+	}
+
 }
